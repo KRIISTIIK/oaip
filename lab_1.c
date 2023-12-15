a6080f216237b63af908f32e38431d0e6745e931
KRIISTIIK <152592248+KRIISTIIK@users.noreply.github.com>
2023-12-15 11:10:59 +0400

    Add files via upload

---
+++ lab_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("LAVROVA KRISTINA \n");
+	printf("PIBD-14\n ");
+	printf("LAB-1\n ");
+	printf("                                \n");
+	printf("     $$$$$$       $$$$$$      \n");
+	printf("     $$$$$$       $$$$$$      \n");
+	printf("   $$$$$$$$       $$$$$$$$      \n");
+	printf("     $$$$$$       $$$$$$      \n");
+	printf("     $$$$$$       $$$$$$      \n");
+	printf("                                \n");
+	printf("             $$$                \n");
+	printf("           $$$$$$$               \n");
+	printf("$$                         $$      \n");
+	printf(" $$$                     $$$    \n");
+	printf("  $$$$$$$$$$$$$$$$$$$$$$$$$      \n");
+}
